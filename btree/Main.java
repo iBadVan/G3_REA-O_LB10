2609b42f99dc63c9235332d26053490695be9bb0
iBadVan <ivan.edgardo.reso@gmail.com>
2025-06-19 01:30:44 -0500

    actu

--- btree/Main.java
+++ btree/Main.java
@@ -1,5 +1,9 @@
 package btree;
 
+import java.io.IOException;
+
+import Exceptions.ItemNoFound;
+
 public class Main {
     public static void main(String[] args) {
         BTree<Integer> tree = new BTree<>(3);
@@ -15,5 +19,15 @@ public class Main {
         System.out.println("\nBúsquedas:");
         System.out.println("¿Está el 52? → Resultado: " + tree.search(52));
         System.out.println("¿Está el 100? → Resultado: " + tree.search(100));
+
+        try {
+            BTree<Integer> tree2 = BTree.building_Btree("arbolB.txt");
+            System.out.println("Árbol B construido correctamente:");
+            tree2.printTree();
+        } catch (ItemNoFound e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Archivo no encontrado o error de lectura.");
+        }
     }
 }
