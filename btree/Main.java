f034dea5e3c79a4733f16e02637ac2548d87219c
iBadVan <ivan.edgardo.reso@gmail.com>
2025-06-19 00:50:23 -0500

    Update Main.java

--- btree/Main.java
+++ btree/Main.java
@@ -2,6 +2,18 @@ package btree;
 
 public class Main {
     public static void main(String[] args) {
-        
+        BTree<Integer> tree = new BTree<>(3);
+
+        int[] valores = {10, 20, 5, 6, 12, 30, 7, 17, 52, 2};
+        for (int val : valores) {
+            tree.insert(val);
+        }
+
+        System.out.println("Contenido del árbol:");
+        tree.printTree();
+
+        System.out.println("\nBúsquedas:");
+        System.out.println("¿Está el 52? → Resultado: " + tree.search(52));
+        System.out.println("¿Está el 100? → Resultado: " + tree.search(100));
     }
 }
