118a44f52aa17220c2788f7e0db0a1fa2c58da33
iBadVan <ivan.edgardo.reso@gmail.com>
2025-06-19 00:05:55 -0500

    Update BNode.java

--- btree/BNode.java
+++ btree/BNode.java
@@ -2,6 +2,8 @@ package btree;
 
 import java.util.ArrayList;
 
+import javax.naming.directory.SearchResult;
+
 public class BNode<E extends Comparable<E>> {
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
@@ -24,6 +26,33 @@ public class BNode<E extends Comparable<E>> {
 
     public boolean nodeEmpty() {
         return count == 0;
+    }
+
+    public SearchResult searchNode(E key) {
+        int i = 0;
+        while (i < count && key.compareTo(keys.get(i)) > 0) {
+            i++;
+        }
+        if (i < count && key.compareTo(keys.get(i)) == 0) {
+            return new SearchResult(true, i);
+        } else {
+            return new SearchResult(false, i); 
+        }
+    }
+
+    public class SearchResult {
+        public boolean found;
+        public int position;
+
+        public SearchResult(boolean found, int position) {
+            this.found = found;
+            this.position = position;
+        }
+
+        @Override
+        public String toString() {
+            return found ? "Key found at position " + position : "Key not found, descend to child " + position;
+        }
     }
 
 }
