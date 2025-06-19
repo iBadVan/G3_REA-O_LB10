0d91b0a6610ec163de465120357366e21ad6d093
iBadVan <ivan.edgardo.reso@gmail.com>
2025-06-19 00:04:40 -0500

    Update BNode.java

--- btree/BNode.java
+++ btree/BNode.java
@@ -1,3 +1,22 @@
 package btree;
 
-public class BNode<E extends Comparable<E>> {}
+import java.util.ArrayList;
+
+public class BNode<E extends Comparable<E>> {
+    protected ArrayList<E> keys;
+    protected ArrayList<BNode<E>> childs;
+    protected int count;
+
+    public BNode(int n) {
+        this.keys = new ArrayList<E>(n);
+        this.childs = new ArrayList<BNode<E>>(n + 1);
+        this.count = 0;
+        for (int i = 0; i < n; i++) {
+            this.keys.add(null);
+            this.childs.add(null);
+        }
+        this.childs.add(null);
+    }
+
+
+}
